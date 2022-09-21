918c671a12549ffa6ad938a89e9f4971c5286108
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-09-21 21:20:45 +0300

    js hw-7-1 full(creat escep)

---
+++ js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+//  hw_7-1
+const gallery = document.querySelector('.gallery');
+
+const galleryCardMarkup = createGalleryCardMarkup(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryCardMarkup);
+
+gallery.addEventListener('click', onModalGalleryClick);
+
+function createGalleryCardMarkup(galleryCards) {
+    return galleryCards.map(({ preview, original, description }) => {        
+        return ` <div class="gallery__item">
+        <a class="gallery__link" href='${original}'>
+            <img
+                class="gallery__image"
+                src='${preview} ' 
+                data-source='${original}' 
+                alt='${description}'
+                />
+        </a>
+    </div>`;        
+    }).join('')  
+}
+
+let instance;
+function onModalGalleryClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.dataset.source) { return };
+    // if (evt.target === evt.currentTarget) { return };
+   
+    const parent = evt.target.closest('.gallery__image');
+    console.log('parent', parent.dataset.source);
+
+    instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" alt='${evt.target.alt}'>`);
+    
+    instance.show();
+    window.addEventListener('keydown', onEscModalOpen);
+    setTimeout(() => {
+        instance.close(), 
+        window.removeEventListener('keydown', onEscModalOpen)
+    }, 5000);
+
+};
+
+function onEscModalOpen(evt) {
+    
+    if (evt.code === 'Escape') {
+        instance.close();
+        window.removeEventListener('keydown', onEscModalOpen)
+    };
+    // console.log('evt2', evt);
+}
+
+
+
